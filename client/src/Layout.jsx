d4792dc89fb3c1823405a5bc713636a4ac47e745
Hemanth Mandapati <hemanth.mandapati01@gmail.com>
2023-06-29 23:47:35 -0700

    Intial Commit for Login and Register functionality

---
+++ client/src/Layout.jsx
@@ -0,0 +1,9 @@
+import Header from "./Header.jsx";
+
+export default function Layout() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,3 @@
+export default function () {
+    return "Login form should go here";
+}
